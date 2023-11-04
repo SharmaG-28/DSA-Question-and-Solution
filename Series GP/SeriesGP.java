07ec227e22f6da66d68846274039799813e4faee
Gautam Sharma <sharmagautam2811@gmail.com>
2023-11-04 23:24:00 +0530

    Series GP -> Question and Answer

--- Series GP/SeriesGP.java
+++ Series GP/SeriesGP.java
@@ -1,24 +1,27 @@
 //Driver Code Starts
 import java.io.*;
-import java.lang.*;
-import java.util.*;
 
 class SeriesGP
 {
     public static void main(String args[])throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+        
+        //Take input a, r, n in simultaneously
         String s1 = br.readLine().trim();
         String[] s2 = s1.split(" ");
+        
         int a = Integer.parseInt(s2[0]);
         int r = Integer.parseInt(s2[1]);
         int n = Integer.parseInt(s2[2]);
 
+        //Instance of Solution class is created
         Solution ob = new Solution();
         System.out.println(ob.Nth_term(a, r, n));
     }
 }
+//Driver Code Ends
 
+//Solution class with Nth_term method
 class Solution
 {
     int Nth_term(int a, int r, int n)
@@ -30,3 +33,5 @@ class Solution
         return ans;
     }
 }
+
+//Code Ends
