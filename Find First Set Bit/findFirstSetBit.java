9c05ccabadc7081a939ff19b5a62e3dfe3a6f3e8
Gautam Sharma <sharmagautam2811@gmail.com>
2024-01-25 23:54:30 +0530

    Find First Set Bit -> problem and solution

---
+++ Find First Set Bit/findFirstSetBit.java
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class findFirstSetBit {
+    
+    
+	public static void main (String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();//taking testcases
+		while(t-->0){
+			int n=sc.nextInt();//input n
+			Solution ob=new Solution();
+			System.out.println(ob.getFirstSetBit(n));//calling method
+		}
+	}
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    //Function to find position of first set bit in the given number.
+    public static int getFirstSetBit(int n){
+            
+        // Your code here
+        if (n == 0) {
+            return 0;
+        }
+        
+        int position = 1;
+        
+        while ((n & 1) != 1) {
+            n = n >> 1;
+            position++;
+        }
+
+        return position;    
+    }
+}
