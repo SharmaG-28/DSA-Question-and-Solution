955612ffe905a95d79460cb983b889917b4d6385
Gautam Sharma <sharmagautam2811@gmail.com>
2024-02-03 23:15:36 +0530

    Boolean Matrix -> Solution

---
+++ Boolean Matrix/booleanMatrix.java
@@ -0,0 +1,85 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class booleanMatrix
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        
+        while(t-- > 0)
+        {
+            String str[] = read.readLine().trim().split("\\s+");
+            int r = Integer.parseInt(str[0]);
+            int c = Integer.parseInt(str[1]);
+            int matrix[][] = new int[r][c];
+            
+            for(int i = 0; i < r; i++)
+            {
+                int k = 0;
+                str = read.readLine().trim().split("\\s+");
+                for(int j = 0; j < c; j++){
+                  matrix[i][j] = Integer.parseInt(str[k]);
+                  k++;
+                }
+            }
+            new Solution().booleanMatrix(matrix);
+            StringBuilder sb = new StringBuilder();
+            for(int i = 0; i < r; i++){
+                for(int j = 0; j < c; j++){
+                    sb.append(matrix[i][j] + " ");
+                }
+                sb.append("\n"); 
+            }
+            System.out.print(sb);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    //Function to modify the matrix such that if a matrix cell matrix[i][j]
+    //is 1 then all the cells in its ith row and jth column will become 1.
+    void booleanMatrix(int matrix[][])
+    {
+        // code here 
+        int r = matrix.length;
+        int c = matrix[0].length;
+        
+        boolean[] rowFlag = new boolean[r];
+        boolean[] colFlag = new boolean[c];
+        
+        for(int i=0; i<r; i++){
+            for(int j=0; j<c; j++){
+                if (matrix[i][j] == 1) {
+                    rowFlag[i] = true;
+                    colFlag[j] = true;
+                }
+            }
+        }
+        
+        for (int i = 0; i<r; i++) {
+            if (rowFlag[i]) {
+                for (int j=0; j<c; j++) {
+                    matrix[i][j] = 1;
+                }
+            }
+        }
+        for (int j=0; j<c; j++) {
+            if (colFlag[j]) {
+                for (int i=0; i<r; i++) {
+                    matrix[i][j] = 1;
+                }
+            }
+        }
+    }
+}
