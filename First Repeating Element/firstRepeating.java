cea1e2f34039ac512ba3d4f4df6b9a066a81f949
Gautam Sharma <sharmagautam2811@gmail.com>
2024-02-04 23:22:52 +0530

    First Repeating Element -> Solution

---
+++ First Repeating Element/firstRepeating.java
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+class firstRepeating {
+    public static void main(String[] args) {
+
+        // Taking input using class Scanner
+        Scanner sc = new Scanner(System.in);
+
+        // Taking total count of testcases
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            // taking total count of elements
+            int n = sc.nextInt();
+
+            // creating a new array of size n
+            int arr[] = new int[n];
+
+            // inserting elements to the array
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+            Solution ob = new Solution();
+            // calling firstRepeated method
+            // and printing the result
+            System.out.println(ob.firstRepeated(arr, n));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    // Function to return the position of the first repeating element.
+    public static int firstRepeated(int[] arr, int n) {
+        // Your code here
+        HashMap<Integer, Integer> indexMap = new HashMap<>();
+        int minIndex = Integer.MAX_VALUE;
+        
+        for (int i = 0; i < n; i++){
+            int element = arr[i];
+            if (!indexMap.containsKey(element)){
+                indexMap.put(element, i + 1);
+            }
+            else{
+                if (indexMap.get(element) < minIndex) {
+                    minIndex = indexMap.get(element);
+                }
+            }
+        }
+        if (minIndex != Integer.MAX_VALUE){
+            return minIndex;
+        }
+        else{
+            return -1;
+        }
+    }
+}
