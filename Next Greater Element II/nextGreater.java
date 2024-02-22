98b0617a4c67b4aba62a77b0fd078fb345e5d0e1
Gautam Sharma <sharmagautam2811@gmail.com>
2024-02-22 22:35:42 +0530

    Next Greater Element II -> Solution

---
+++ Next Greater Element II/nextGreater.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+class Solution {
+    public int[] nextGreaterElements(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+        Arrays.fill(result, -1);
+        Deque<Integer> stack = new ArrayDeque<>();
+
+        for (int i = 0; i < 2 * n; i++) {
+            int num = nums[i % n];
+            while (!stack.isEmpty() && nums[stack.peek()] < num) {
+                result[stack.pop()] = num;
+            }
+            if (i < n) {
+                stack.push(i);
+            }
+        }
+
+        return result;
+    }
+}
+
+public class nextGreater {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number of elements in the array:");
+        int n = scanner.nextInt();
+        int[] nums = new int[n];
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < n; i++) {
+            nums[i] = scanner.nextInt();
+        }
+
+        Solution solution = new Solution();
+        int[] result = solution.nextGreaterElements(nums);
+
+        System.out.println("The next greater elements for each element in the array are:");
+        for (int num : result) {
+            System.out.print(num + " ");
+        }
+    }
+}
